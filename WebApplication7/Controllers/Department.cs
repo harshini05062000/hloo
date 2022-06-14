6bea613bcf56aaa5fd44c4e1d7b26b8fc87d4797
harshini05062000 <harshinisanisetty@gmail.com>
2022-06-14 16:52:16 +0530

    Add project files.

---
+++ WebApplication7/Controllers/Department.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class Department : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DeptDetails()
+        {
+            Dept dobj = new Dept { deptno = 100, dname = "Sales", location = "Mumbai" };
+            return View(dobj);
+        }
+    }
+}
---
+++ WebApplication7/Models/Dept.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Dept
+    {
+        public int deptno { get; set; }
+        public string dname { get; set; }
+        public string location { get; set; }
+    }
+}
